f4a6f84bf75714660f8b895d5ed22f1d47ca7a52
jy006 <yehezkieljason@gmail.com>
2018-04-30 01:44:18 +0700

    fix kelurahan

--- PageAdminKelurahan.php
+++ PageAdminKelurahan.php
@@ -17,46 +17,45 @@
 
         <form action="" method="post">
             <?php
-            $idkel = $_POST['rahasia'];
+            $idKel = $_POST['rahasia'];
             if(isset($_POST['edit'])){
-                $namakel = $_POST['outNamakel'];
-                $query2 = "UPDATE kelurahan SET namakelurahan = '$namakel' WHERE idkelurahan = $idkel";
+                $namaKel = $_POST['outNamakel'];
+                $query2 = "UPDATE kelurahan SET namaKelurahan = '$namaKel' WHERE idKelurahan = $idKel";
                 $con->query($query2);
             }
 
             if(isset($_POST['delete'])){
-                $query4 = "DELETE FROM kelurahan WHERE idkelurahan = $idkel";
+                $query4 = "DELETE FROM Kelurahan WHERE idKelurahan = $idkel";
                 $con->query($query4);
             }
             
             if(isset($_POST['insertButton'])){
                 $insertName = $_POST['insertNamakel'];
-                $query3 = "INSERT INTO kelurahan(namakelurahan) VALUES ('$insertName')";
+                $query3 = "INSERT INTO kelurahan(namaKelurahan) VALUES ('$insertName')";
                 $con->query($query3);
             }
             $result = $con->query($query);
-                echo "<h1>List kelurahan</h1>";
+                echo "<h1>List Kelurahan</h1>";
                 echo '<label>Nama kelurahan: </label><input id="insertNamakel" type="text" name="insertNamakel">';
                 echo '<input type="submit" value="Insert kelurahan" id="insertButton" name="insertButton">';
                 echo "<table>";
                 echo "<tr>";
-                echo "<th>idkelurahan</th>";
-                echo "<th>Nama kelurahan</th>";
-                echo "<th>Ganti Nama kelurahan</th>";
+                echo "<th>idKelurahan</th>";
+                echo "<th>Nama Kelurahan</th>";
+                echo "<th>Ganti Nama Kelurahan</th>";
                 echo "<th></th>";
                 echo "<th></th>";
                 echo "</tr>";
                 while($row = $result->fetch_array()){
                     echo "<tr>";
                     echo '<form method='."post".'>';
-                    echo '<td>'.$row["idkelurahan"].'</td>';
-                    echo "<td>".$row['namakelurahan']."</td>";
-                    $currNamakelurahan = $row['namakelurahan'];
-                    echo "<td><input type='text' name='outNamakel' value=".$row['namakelurahan'].">";
+                    echo '<td>'.$row["idKelurahan"].'</td>';
+                    echo "<td>".$row['namaKelurahan']."</td>";
+                    echo "<td><input type='text' name='outNamaKel' value=".$row['namaKelurahan'].">";
                     echo '<td><input type="submit" name="edit" value="EDIT">';
                     echo '<td><input type="submit" name="delete" value="DELETE">';
-                    echo '<input id="rahasia" type="text" value='.$row["idkelurahan"].' name="rahasia">';
-                    echo '<input id="rahasia" type="text" value='.$row["namakelurahan"].' name="rahasia2">';
+                    echo '<input id="rahasia" type="text" value='.$row["idKelurahan"].' name="rahasia">';
+                    echo '<input id="rahasia" type="text" value='.$row["namaKelurahan"].' name="rahasia2">';
                     echo "</form>";
                 }
                 echo "</form>";
